64e8b6d3df890fd0f1861a318c3a2a10b4b4731e
Ryo028 <miyata028@gmail.com>
2017-02-11 22:13:31 +0900

    テスト

--- FirebaseSample/PreViewController.swift
+++ FirebaseSample/PreViewController.swift
@@ -12,7 +12,7 @@ class PreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("preview")
         // Do any additional setup after loading the view.
     }
 
